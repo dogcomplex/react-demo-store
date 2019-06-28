a202af5d00b19c52ca3c49388fdeedc1891cee87
Warren Koch <warrenkoch@gmail.com>
2019-06-28 12:47:31 -0700

    AllProducts Minor Cleanup
    Fail first, avoid nesting

--- src/components/Products/AllProducts.js
+++ src/components/Products/AllProducts.js
@@ -14,47 +14,13 @@ const isThereACurrencyPrice = product => {
   }
 };
 
-const AllProducts = props => {
-  if (props.css !== null && props.products.products.data.length > 0) {
-    var products = props.products.products;
-
+const AllProducts = ({ products: { products }, css }) => {
+  if (css === null || products.data.length <= 0) {
     return (
       <main role="main" id="container" className="main-container push">
         <section className="products">
           <div className="content">
-            <div className="product-list">
-              {products.data.map(function(product) {
-                let background;
-                if (product.background_colour) {
-                  background = product.background_colour;
-                } else {
-                  background = '#d9d9d9';
-                }
-
-                return (
-                  <a
-                    className="product-item"
-                    href={'/product/' + product.id}
-                    key={product.id}>
-                    <div
-                      className="product-image"
-                      style={{ background: background }}>
-                      <ProductImage product={product} products={products} />
-                    </div>
-                    <div className="overlay">
-                      <div
-                        className="overlay-background"
-                        style={{ background: '#aaaaaa' }}
-                      />
-                      <div className="overlay-content">
-                        <div className="title">{product.name}</div>
-                        {isThereACurrencyPrice(product)}
-                      </div>
-                    </div>
-                  </a>
-                );
-              })}
-            </div>
+            <p>You do not have any products</p>
           </div>
         </section>
       </main>
@@ -65,7 +31,34 @@ const AllProducts = props => {
     <main role="main" id="container" className="main-container push">
       <section className="products">
         <div className="content">
-          <p>You do not have any products</p>
+          <div className="product-list">
+            {products.data.map(product => (
+              <a
+                className="product-item"
+                href={'/product/' + product.id}
+                key={product.id}>
+                <div
+                  className="product-image"
+                  style={{
+                    background: product.background_colour
+                      ? product.background_colour
+                      : '#d9d9d9'
+                  }}>
+                  <ProductImage product={product} products={products} />
+                </div>
+                <div className="overlay">
+                  <div
+                    className="overlay-background"
+                    style={{ background: '#aaaaaa' }}
+                  />
+                  <div className="overlay-content">
+                    <div className="title">{product.name}</div>
+                    {isThereACurrencyPrice(product)}
+                  </div>
+                </div>
+              </a>
+            ))}
+          </div>
         </div>
       </section>
     </main>
